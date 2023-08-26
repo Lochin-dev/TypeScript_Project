087fc27e3476aa0078fc1c7127542c020c6182e3
web-developer001 <lochinquvondiqov31@gmail.com>
2023-08-26 20:56:46 +0500

    home

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import { Container } from "react-bootstrap";
-import { Home } from "./pages/home";
+import Home from "./pages/Home";
 import { Store } from "./pages/Store";
 import { About } from "./pages/About";
 import { Navbar } from "./components/Navbar";
